c013488f365e35b82a7b4d9e8a7963e25caee966
hullymully <94529090+HullyMully@users.noreply.github.com>
2025-04-23 22:14:04 +0600

    Исправлена конфигурация маршрутизации Vercel

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://omsu-coin-9vxpzl2pr-hullymullys-projects.vercel.app'
+  ? 'https://omsu-coin-hpy7dgqn5-hullymullys-projects.vercel.app'
   : 'http://localhost:5000';
 
 const api = axios.create({
